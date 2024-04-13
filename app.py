cdcf9b7101e26ff68efed69bd9a227be02163b6f
GeoffNderitu <geoffreynderitun@gmail.com>
2024-04-13 23:22:47 +0300

    Input from user

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-patient_f_name = "John Smith"
-age = 20
-patient_type = "new patient"
-print("His name is",patient_f_name,"He is",age,"He is a",patient_type)
+# Variables
+# patient_f_name = "John Smith"
+# age = 20
+# patient_type = "new patient"
+# print("His name is",patient_f_name,"He is",age,"He is a",patient_type)
+
+# Input Values
+input("What is your name?")
